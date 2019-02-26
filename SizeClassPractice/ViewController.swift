535ea81e2245e82d70bde9f8c813c7d1a7a8fc2e
28cmm <joshuafang1998@gmail.com>
2019-02-25 18:57:49 -0800

    first part

---
+++ SizeClassPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SizeClassPractice
+//
+//  Created by Yilei Huang on 2019-02-25.
+//  Copyright © 2019 Joshua Fang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
